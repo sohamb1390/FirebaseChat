3bedefeb83c733befe82dc069d0959a641bb0a0c
Vaggish Vasu Rachamalla <vaggish.vasu.rachamalla@citi.com>
2016-10-14 13:48:45 +0530

    Initial Commit

---
+++ LocationChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationChat
+//
+//  Created by Soham Bhattacharjee on 14/10/16.
+//  Copyright © 2016 Soham Bhattacharjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
